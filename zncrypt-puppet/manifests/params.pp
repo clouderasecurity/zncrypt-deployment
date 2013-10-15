382e4f915f1396fa4e2fc63c3d748e2794dc42fe
Darin Perusich <darin@darins.net>
2013-10-15 12:03:15 -0400

    configuration settings for zncrypt

---
+++ zncrypt-puppet/manifests/params.pp
@@ -0,0 +1,9 @@
+class zncrypt::params {
+    $keyserver          = ""
+    $activation_email   = ""
+    $passphrase         = ""
+    $passphrase2        = ""
+    $zncrypt_prefix     = "/var/lib/zncrypt"
+    $zncrypt_mount      = "${zncrypt_mount}/mnt"
+    $zncrypt_storage    = "${zncrypt_mount}/storage"
+}
