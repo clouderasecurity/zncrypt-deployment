03ba277afd6115808d767f0c6e356369f2fef307
Darin Perusich <darin@darins.net>
2013-10-15 12:37:56 -0400

    moved into params

--- zncrypt-puppet/manifests/init.pp
+++ zncrypt-puppet/manifests/init.pp
@@ -1,94 +1,12 @@
-class zncrypt {
-
-    ###############################################################
-    # Hard-coded values
-    #
-    # IMPORTANT: Do NOT leave the passphrase and/or passphrase2 in
-    #   this script!  We recommend generating a strong random
-    #   passphrase or alternatively using an RSA key
-    ###############################################################
-
-    # zTrustee Key Server to register against
-    $keyserver = ""
-    # Email address to use for activation request
-    $activation_email = ""
-    
-    # Example master passphrases
-    $passphrase = ""
-    $passphrase2 = ""
-    
-    # zNcrypt directories that are required for operation
-    $zncrypt_prefix = "/var/lib/zncrypt"
-    # The mount-point for the encrypted file system
-    $zncrypt_mount = "/var/lib/zncrypt/mnt"
-    # The location where the encrypted file system will be stored
-    $zncrypt_storage = "/var/lib/zncrypt/storage"
-
-    ###############################################################
-    # Set variables or execute commands that depend on the OS
-    ###############################################################
-
-    case $::operatingsystem 
-    {
-        'CentOS': 
-        {
-            yumrepo 
-            { 
-                "epel":
-                descr => 'EPEL',
-                mirrorlist => 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-$releasever&arch=$basearch',
-                enabled => 1,
-                gpgcheck => 1,
-                gpgkey => "https://fedoraproject.org/static/0608B895.txt";
-                "gazzang":
-                descr  => 'Gazzang',
-                baseurl => 'http://archive.gazzang.com/redhat/stable/$releasever',
-                enabled => 1,
-                gpgcheck => 1,
-                gpgkey => "http://archive.gazzang.com/gpg_gazzang.asc",
-            }
-            package 
-            { 
-                ["kernel-devel","kernel-headers","dkms","zncrypt"]:
-                ensure => present,
-                require => Yumrepo[ "epel", "gazzang" ]
-            }
-        }
-        'Ubuntu': 
-        {
-            include apt
-            apt::key 
-            { 
-                "gazzang":
-                ensure => present,
-                key_source => 'http://archive.gazzang.com/gpg_gazzang.asc',
-            }
-            apt::source 
-            { 
-                "gazzang":
-                location      	=> "http://archive.gazzang.com/ubuntu/stable",
-                release       	=> $lsbdistcodenamestable,
-                key_source    	=> 'http://archive.gazzang.com/gpg_gazzang.asc',
-                include_src   	=> false,
-            }
-
-            # Ensure apt is setup before running apt-get update
-            Apt::Key <| |> -> Exec["apt-update"]
-            Apt::Source <| |> -> Exec["apt-update"]
-
-            # Ensure apt-get update has been run before installing any packages
-            Exec["apt-update"] -> Package <| |>
-
-            exec { "apt-update":
-                command 	=> "/usr/bin/apt-get update",
-                refreshonly => true,
-            }
-            package { 
-                ["linux-headers-$kernelrelease","dkms","zncrypt"]:
-                ensure => latest,
-            }
-        }
-    }
+class zncrypt (
+    $keyserver          = $zncrypt::params::keyserver,
+    $activation_email   = $zncrypt::params::activation_email,
+    $passphrase         = $zncrypt::params::passphrase,
+    $passphrase2        = $zncrypt::params::passphrase2,
+    $zncrypt_prefix     = $zncrypt::params::zncrypt_prefix,
+    $zncrypt_mount      = $zncrypt::params::zncrypt_mount,
+    $zncrypt_storage    = $zncrypt::params::zncrypt_storage
+    ) inherits zncrypt::params {
 
     # Create necessary directories
     file { [$zncrypt_prefix, $zncrypt_mount, $zncrypt_storage]:
