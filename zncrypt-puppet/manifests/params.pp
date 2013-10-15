d0d86c6f2a5d6c6fb2923b47ec89af9313a1a143
Darin Perusich <darin@darins.net>
2013-10-15 12:24:38 -0400

    passphrases are required

--- zncrypt-puppet/manifests/params.pp
+++ zncrypt-puppet/manifests/params.pp
@@ -13,8 +13,8 @@
 class zncrypt::params {
     $keyserver
     $activation_email
-    $passphrase         = ""
-    $passphrase2        = ""
+    $passphrase
+    $passphrase2
     $zncrypt_prefix     = "/var/lib/zncrypt"
     $zncrypt_mount      = "${zncrypt_mount}/mnt"
     $zncrypt_storage    = "${zncrypt_mount}/storage"
