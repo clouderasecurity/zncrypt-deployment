b6a5c6178eb60cc26717c72638c6b37dc3a8005c
Ross McDonald <ross.mcdonald@gazzang.com>
2014-03-12 13:49:08 -0500

    Added reads from /dev/tty so that script can be piped through bash.

--- configure-zncrypt.sh
+++ configure-zncrypt.sh
@@ -50,14 +50,14 @@ function displayzNcryptPartitions {
 function registerClient {
     test ! -f /etc/zncrypt/ztrustee/clientname || return
     printf "\nWhat zTrustee Server would you like to use? [ztdemo.gazzang.net]\n"
-    read keyserver
+    read keyserver < /dev/tty
     test ! -z $keyserver || keyserver="ztdemo.gazzang.net"
     verifyConnectivity $keyserver
 
     printf "\n\nWhat organization would you like to register against? []\n"
-    read org
+    read org < /dev/tty
     printf "\nWhat is the authorization code for '$org' organization? []\n"
-    read auth
+    read auth < /dev/tty
 
     register_command="zncrypt register -s $keyserver -t single-passphrase"
     # Test for 0 length strings to maintain compatibility with classic reg mode
@@ -77,20 +77,22 @@ function registerClient {
 
 function prepareClient {
     printf "\nDo you need to prepare any drives/directories for encryption? [no]\n"
-    read response
+    read response < /dev/tty
     test "${response:0:1}" = "y" || test "${response:0:1}" = "Y" || return
     unset response
     
     printf "\nWhere would you like to store the encrypted data? [/var/lib/zncrypt/.private]\n"
-    read storage
+    read storage < /dev/tty
     test -z $storage && storage="/var/lib/zncrypt/.private"
+    grep "$storage" /etc/zncrypt/ztab &>/dev/null && err "The location '$storage' is already marked as an encrypted partition."
     test -L $storage && storage="$(ls $storage | xargs readlink -f)" && printf "*You specified a symbolic link. Setting new encryption target to '$storage'.\n"
     test -b $storage && err "Sorry, block-level encryption is not support by this script (yet)."
     test -d $storage || mkdir -p $storage
     
     printf "\nAnd where would you like to mount the encrypted data? [/var/lib/zncrypt/encrypted]\n"
-    read mount
+    read mount < /dev/tty
     test -z $mount && mount="/var/lib/zncrypt/encrypted"
+    grep "$mount" /etc/zncrypt/ztab &>/dev/null && err "The location '$mount' is already marked as an encrypted partition."
     test -d $mount || mkdir -p $mount
     
     prepare_command="zncrypt-prepare $storage $mount"
@@ -107,12 +109,12 @@ function prepareClient {
 
 function encryptData {
     printf "\nDo you want to encrypt any data? [no]\n"
-    read response
+    read response < /dev/tty
     test "${response:0:1}" = "y" || test "${response:0:1}" = "Y" || return
     unset response
     
     printf "\nWhat data would you like to encrypt? This can be either a directory or just a file.\n"
-    read to_encrypt
+    read to_encrypt < /dev/tty
     test -z $to_encrypt && err "A valid directory or file location must be specified."
     test -L $to_encrypt && to_encrypt="$(ls $to_encrypt | xargs readlink -f)" && printf "*You specified a symbolic link. Setting new encryption target to '$to_encrypt'.\n"
     test -d $to_encrypt || test -f $to_encrypt || err "A valid directory or file location must be specified."
@@ -120,16 +122,16 @@ function encryptData {
     if [[ -z $mount ]]; then
         displayzNcryptPartitions
         printf "\nWhat mount location would you like to use? []\n"
-        read mount
+        read mount < /dev/tty
         test -z $mount && err "A valid encrypted partition must be specified."
     else
         printf "\nYou specified the mount location '$mount' from before. Would you like to use that location to store this encrypted data? [yes]\n"
-        read response
+        read response < /dev/tty
         test -z $response && response="yes"
         if [["${response:0:1}" = "n"] -o ["${response:0:1}" = "N"]]; then
             displayzNcryptPartitions
             printf "\nWhat mount location would you like to use? []\n"
-            read mount
+            read mount < /dev/tty
             grep "$mount" /etc/zncrypt/ztab &>/dev/null || err "Need to specify a valid encrypted mount-point."
         fi
         unset response
@@ -137,7 +139,7 @@ function encryptData {
     test -z $mount && err "You need to specify a valid mount location."
     
     printf "\nWhat category name would you like to encrypt this data with? [encrypted]\n"
-    read category
+    read category < /dev/tty
     test -z $category && category="encrypted"
     
     encrypt_command="zncrypt-move encrypt @$category $to_encrypt $mount"
@@ -154,29 +156,30 @@ function encryptData {
 
 function addRules {
     printf "\nDo you want to set any ACL rules? [no]\n"
-    read response
+    read response < /dev/tty
     test "${response:0:1}" = "y" || test "${response:0:1}" = "Y" || return
     unset response
     
     printf "\nWhat binary would you like to allow access to the encrypted data?\n"
-    read binary
+    read binary < /dev/tty
     test -z $binary && err "Please specify a valid binary."
+
     test -L $binary && binary="$(ls $binary | xargs readlink -f)" && printf "*You specified a symbolic link. Setting new binary target to '$binary'.\n"
     test -x $binary || err "A valid executable must be specified."
     
     if [[ -z $category ]]; then
         printf "\nWhat category name would you like to set for this rule? [encrypted]\n"
-        read category
+        read category < /dev/tty
         test -z $category && category="encrypted"
     else
         printf "\nYou used the category name '$category' before. Would you like to use the same name? [yes]\n"
-        read response
+        read response < /dev/tty
         test -z $response && response="yes"
         test ${response:0:1} = "n" || test ${response:0:1} = "N" || break
         unset response
         
         printf "\nWhat category name would you like to set for this rule? [encrypted]\n"
-        read category
+        read category < /dev/tty
         test -z $category && category="encrypted"
     fi
     
